5fc84f223480672173d8c99890929c3a380d6d80
TopsterTX <bbs.100@yandex.ru>
2022-05-22 17:36:36 +0300

    [feature] added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default () => {
+  return (
+    <>
+      <h1>404</h1>
+      <p>
+        Please <Link href="/">go back</Link> to safety
+      </p>
+    </>
+  );
+};
